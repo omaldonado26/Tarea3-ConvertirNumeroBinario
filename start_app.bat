44c2aa5714a761be147113189ad451b749d58401
omaldonado26 <oemaldonado@uth.hn>
2025-07-23 16:04:19 -0600

    start

---
+++ start_app.bat
@@ -0,0 +1,17 @@
+@echo off
+REM -----------------------------
+REM Arranca Django y abre navegador
+REM -----------------------------
+cd /d %~dp0
+
+echo Activando entorno virtual...
+call venv\Scripts\activate.bat
+
+echo Aplicando migraciones...
+python manage.py migrate
+
+echo Abriendo navegador...
+start http://127.0.0.1:8000/
+
+echo Iniciando servidor de desarrollo...
+python manage.py runserver
